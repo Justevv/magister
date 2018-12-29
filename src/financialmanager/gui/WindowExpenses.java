ff86d6b5ac79e9ff5e8c7d32c9fa27126d3d3df0
vtychkov <viktychkov@gmail.com>
2018-12-29 08:21:03 +0300

    добавлен коментарий

--- src/financialmanager/gui/WindowExpenses.java
+++ src/financialmanager/gui/WindowExpenses.java
@@ -30,6 +30,7 @@ public class WindowExpenses extends JFrame {
     static JScrollPane jscrlp = new JScrollPane(jTabPeople);
     final static boolean shouldWeightX = true;
 //    int nullcolumn;
+    //Проверка гита
 
     public WindowExpenses() {
 
