c97554cd09ef015f7784dd9d29933576ba27f7dd
tychkov <aleksandr.tychkov@loginom.ru>
2018-12-29 10:13:16 +0300

    test2

--- src/financialmanager/gui/WindowExpenses.java
+++ src/financialmanager/gui/WindowExpenses.java
@@ -30,7 +30,6 @@ public class WindowExpenses extends JFrame {
     static JScrollPane jscrlp = new JScrollPane(jTabPeople);
     final static boolean shouldWeightX = true;
 //    int nullcolumn;
-    //test 1
 
     public WindowExpenses() {
 
