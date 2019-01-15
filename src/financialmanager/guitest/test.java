42c6dbd1462161444399c545d7819a935e1bc0ae
tychkov <aleksandr.tychkov@loginom.ru>
2019-01-15 14:16:22 +0300

    исправлена невозможность редактирования если не изменялся Email

--- src/financialmanager/guitest/test.java
+++ src/financialmanager/guitest/test.java
@@ -1,12 +1,20 @@
 package financialmanager.guitest;
 
-import java.util.Arrays;
-
 public class test {
     public static void main(String[] args) {
 
+        if (4==4){
+            if (5==6){
+                System.out.println("5");
+            }
+        } else {
+            System.out.println("6");
+        }
 
-    String[] values = {"AB","BC","CD","AE"};
-    boolean contains = Arrays.stream(values).anyMatch("CD"::equals);
-        System.out.println(contains);
+
+
+
+//    String[] values = {"AB","BC","CD","AE"};
+//    boolean contains = Arrays.stream(values).anyMatch("CD"::equals);
+//        System.out.println(contains);
 }}
